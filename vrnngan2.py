d196e4cd07fa134f6994e60079ef469d50c5a25c
Justin <j.ch.lee118@gmail.com>
2021-12-26 22:15:34 -0500

    changes

--- vrnngan2.py
+++ vrnngan2.py
@@ -277,17 +277,14 @@ class VRNNGRUGAN(tf.keras.Model):
 		self.vrnn = vrnn_model
 				
 		disc_input = keras.layers.Input(shape=(timesteps, self.feature_space))
-		# disc_rnn = keras.layers.Bidirectional(keras.layers.GRU(32, recurrent_dropout=0.5, dropout=0.5), merge_mode='ave', name='feature_ext')(disc_input)
+		# disc_rnn = keras.layers.Bidirectional(keras.layers.GRU(16, recurrent_dropout=0.5, dropout=0.5), merge_mode='ave', name='feature_ext')(disc_input)
 
-		disc_rnn = tfa.layers.SpectralNormalization(keras.layers.Conv1D(filters=12, kernel_size=10, activation='relu'))(disc_input)
+		disc_rnn = tfa.layers.SpectralNormalization(keras.layers.Conv1D(filters=8, kernel_size=3, activation='relu'))(disc_input)
 		disc_rnn = tf.keras.layers.LeakyReLU(0.2)(disc_rnn)
-		# disc_rnn = tfa.layers.SpectralNormalization(keras.layers.Conv1D(filters=32, kernel_size=16, activation='relu'))(disc_input)
-		# disc_rnn = tf.keras.layers.LeakyReLU(0.2)(disc_rnn)
-		disc_rnn = keras.layers.MaxPooling1D(pool_size=1, strides=1, padding='same')(disc_rnn)
-		disc_rnn = keras.layers.Flatten()(disc_rnn)
-		disc_rnn = keras.layers.Dense(8)(disc_rnn)
-		disc_rnn = tf.keras.layers.LeakyReLU(0.2)(disc_rnn)
-		disc_rnn = tf.keras.layers.Dropout(0.2, name='feature_ext')(disc_rnn)
+		disc_rnn = tf.keras.layers.Dropout(0.5)(disc_rnn)
+		disc_rnn = keras.layers.MaxPooling1D(pool_size=2, strides=1, padding='same')(disc_rnn)
+		disc_rnn = keras.layers.Dense(16)(disc_rnn)
+		disc_rnn = keras.layers.LeakyReLU(0.2, name="feature_ext")(disc_rnn)
 		disc_output = keras.layers.Dense(1)(disc_rnn)
 		disc_model = keras.Model(disc_input, disc_output)
 		self.discrim = disc_model
@@ -347,7 +344,7 @@ class VRNNGRUGAN(tf.keras.Model):
 			)
 
 			# discrim_output_loss_fake = tf.reduce_mean(
-			#     wasserstein_loss(-tf.ones_like(discrim_fake_output), discrim_fake_output)
+			#     discrim_fake_output
 			# )
 		
 			discrim_output_loss_real = tf.reduce_mean(
@@ -355,14 +352,12 @@ class VRNNGRUGAN(tf.keras.Model):
 			)
 
 			# discrim_output_loss_real = tf.reduce_mean(
-			#     wasserstein_loss(tf.ones_like(discrim_real_output), discrim_real_output)
+			#     discrim_real_output
 			# )
 
-			# w_loss = tf.reduce_sum(discrim_fake_output) - tf.reduce_sum(discrim_real_output)
-			  
-			discrim_loss = discrim_output_loss_fake + discrim_output_loss_real 
+			discrim_loss = discrim_output_loss_fake + discrim_output_loss_real
 		discrim_grads = tape.gradient(discrim_loss, self.discrim.trainable_weights)
-		# discrim_grads,_ = tf.clip_by_global_norm(discrim_grads, 10.0)
+		# discrim_grads,_ = tf.clip_by_global_norm(discrim_grads)
 		self.discrim_optimizer.apply_gradients(zip(discrim_grads, self.discrim.trainable_weights))
 		self.discrim_loss_tracker.update_state(discrim_loss)
 		self.discrim_fake_loss_tracker.update_state(discrim_output_loss_fake)
@@ -418,9 +413,9 @@ class VRNNGRUGAN(tf.keras.Model):
 				nll_gauss(output_data, preds_mu, preds_std)
 			)
 			
-			# mislead_output_discrim_loss = tf.reduce_mean(
-			#     tf.keras.losses.mean_squared_error(real_embed, fake_embed) 
-			# )
+			mislead_output_discrim_loss = tf.reduce_sum(
+			    tf.keras.losses.mean_squared_error(real_embed, fake_embed) 
+			)
 
 			# mislead_output_discrim_loss = (tf.reduce_mean(real_embed) - tf.reduce_mean(fake_embed))**2
 
@@ -429,16 +424,16 @@ class VRNNGRUGAN(tf.keras.Model):
 			#     wasserstein_loss(-tf.ones_like(discrim_fake_output), discrim_fake_output)
 			# )
 			
-			mislead_output_discrim_loss = tf.reduce_mean(
-				bce(tf.ones_like(discrim_fake_output), discrim_fake_output)
-			)
+			# mislead_output_discrim_loss = tf.reduce_mean(
+			# 	bce(tf.ones_like(discrim_fake_output), discrim_fake_output)
+			# )
 			
 			
 			total_loss =  reconstruction_loss + 2.0 * kl_loss + mislead_output_discrim_loss
 			# total_loss =  self.lambda_gan * mislead_output_discrim_loss
 
 		encoder_grads = tape.gradient(total_loss, self.vrnn.trainable_weights)
-		encoder_grads,_ = tf.clip_by_global_norm(encoder_grads, 1.15)
+		# encoder_grads,_ = tf.clip_by_global_norm(encoder_grads)
 		
 		self.vae_optimizer.apply_gradients(zip(encoder_grads, self.vrnn.trainable_weights))
 
@@ -506,43 +501,245 @@ class VRNNGRUGAN(tf.keras.Model):
 		output, state = self.vrnn_cell(input, states=state, inference=False)
 		return output, state
 
-	def seed_hdi(self, point, state):
-		# 1-dimensional point input
-		means= []
-		stds = []
-		inp = np.reshape(np.repeat(point,1000), (1000,1))
+	# def seed_hdi(self, point, state):
+	# 	# 1-dimensional point input
+	# 	means= []
+	# 	stds = []
+	# 	inp = np.reshape(np.repeat(point,1000), (1000,1))
 
-		outputs, _ = self.vrnn_cell(inp, states=state, inference=True)
-		means = np.squeeze(outputs[-3])
-		stds = np.squeeze(outputs[-2])
+	# 	outputs, _ = self.vrnn_cell(inp, states=state, inference=True)
+	# 	means = np.squeeze(outputs[-3])
+	# 	stds = np.squeeze(outputs[-2])
 
-		means_hdi = az.hdi(np.asarray(means),0.98)
-		stds_hdi = az.hdi(np.asarray(stds), 0.98)
+	# 	means_hdi = az.hdi(np.asarray(means),0.98)
+	# 	stds_hdi = az.hdi(np.asarray(stds), 0.98)
 
-		return means_hdi, stds_hdi
+	# 	return means_hdi, stds_hdi
 
 
-	def cpd(self, array):
-		'''
-		array is a 1-dimensional array of inputs
-		'''
-		cp = []
-		# seed hdi
-		means= []
-		means_hdi, stds_hdi = self.seed_hdi(array[0], state=None)
+	# def cpd(self, array):
+	# 	'''
+	# 	array is a 1-dimensional array of inputs
+	# 	'''
+	# 	cp = []
+	# 	# seed hdi
+	# 	means= []
+	# 	means_hdi, stds_hdi = self.seed_hdi(array[0], state=None)
 
-		state = None
+	# 	state = None
 
-		for i in range(len(array)):
-			inp = np.reshape(np.asarray(array[i]), (1,1))
-			outputs, next_state = self.vrnn_cell(inp,  states=state, inference=True)
-			mean = outputs[-3][0][0]
-			std = outputs[-2][0][0]
+	# 	for i in range(len(array)):
+	# 		inp = np.reshape(np.asarray(array[i]), (1,1))
+	# 		outputs, next_state = self.vrnn_cell(inp,  states=state, inference=True)
+	# 		mean = outputs[-3][0][0]
+	# 		std = outputs[-2][0][0]
 
-			if (mean < means_hdi[0] or mean > means_hdi[1]) and (std < stds_hdi[0] or std > stds_hdi[1]):
-				if state is not None:
-					state = [np.tile(state,[1000,1])]
-				means_hdi, stds_hdi = self.seed_hdi(array[i], state)
-				cp.append(i)
-			state=[next_state]
-		return cp
+	# 		if (mean < means_hdi[0] or mean > means_hdi[1]) and (std < stds_hdi[0] or std > stds_hdi[1]):
+	# 			if state is not None:
+	# 				state = [np.tile(state,[1000,1])]
+	# 			means_hdi, stds_hdi = self.seed_hdi(array[i], state)
+	# 			cp.append(i)
+	# 		state=[next_state]
+	# 	return cp
+
+
+class VRNNWGAN(tf.keras.Model):
+	def __init__(self, feature_space, z_dim, latent_dim, timesteps, lambda_gan, vrnn_model, **kwargs):
+		super(VRNNWGAN, self).__init__(**kwargs)
+		self.lambda_gan = lambda_gan
+		self.feature_space = feature_space
+		self.latent_dim = latent_dim
+		self.z_dim = z_dim
+		self.feature_space = feature_space
+		
+		# vrnn_input = keras.layers.Input(shape=(timesteps, feature_space))
+		# vrnn_output = keras.layers.RNN(self.vrnn_cell, return_sequences=True)(vrnn_input)
+		self.vrnn = vrnn_model
+				
+		disc_input = keras.layers.Input(shape=(timesteps, self.feature_space))
+		# disc_rnn = keras.layers.Bidirectional(keras.layers.GRU(16, recurrent_dropout=0.5, dropout=0.5), merge_mode='ave', name='feature_ext')(disc_input)
+
+		disc_rnn = tfa.layers.SpectralNormalization(keras.layers.Conv1D(filters=8, kernel_size=3, activation='relu'))(disc_input)
+		disc_rnn = tf.keras.layers.LeakyReLU(0.2)(disc_rnn)
+		disc_rnn = tf.keras.layers.Dropout(0.5)(disc_rnn)
+		disc_rnn = keras.layers.MaxPooling1D(pool_size=2, strides=1, padding='same')(disc_rnn)
+		disc_rnn = keras.layers.Dense(16)(disc_rnn)
+		disc_rnn = keras.layers.LeakyReLU(0.2, name="feature_ext")(disc_rnn)
+		disc_output = keras.layers.Dense(1)(disc_rnn)
+		disc_model = keras.Model(disc_input, disc_output)
+		self.discrim = disc_model
+		
+		self.f = keras.Model(disc_input, self.discrim.get_layer('feature_ext').output)
+		
+		self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
+		self.reconstruction_loss_tracker = keras.metrics.Mean(
+			name="reconstruction_loss"
+		)
+
+		self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
+		self.discrim_loss_tracker = keras.metrics.Mean(name="discrim_loss")
+		self.discrim_fake_loss_tracker = keras.metrics.Mean(name="discrim_fake_loss")
+		self.discrim_real_loss_tracker = keras.metrics.Mean(name="discrim_real_loss")
+		
+	@property
+	def metrics(self):
+		return [
+			self.total_loss_tracker,
+			self.reconstruction_loss_tracker,
+			self.kl_loss_tracker,
+			self.discrim_loss_tracker,
+			self.discrim_fake_loss_tracker,
+			self.discrim_real_loss_tracker,
+		]
+	def compile(self, vae_optimizer, discrim_optimizer):
+		super(VRNNWGAN, self).compile()
+		self.vae_optimizer = vae_optimizer
+		self.discrim_optimizer = discrim_optimizer
+
+	def train_step(self, data):
+		if isinstance(data, tuple):
+			input_data = data[0]
+			output_data = data[1]
+
+		with tf.GradientTape(persistent=True) as tape:
+			inputs = input_data[:,0,:]
+			state = None
+			gen = []
+			for i in range(100):
+				# outputs and states should be batch_size, feature_size
+				sample, state = self.vrnn.vrnn_cell(inputs, state, inference=False)
+				gen.append(sample[0])
+				inputs = sample[0] 
+				state = [state]
+			gen = tf.stack(gen, axis=1)
+
+			discrim_fake_output = self.discrim(gen)
+			discrim_real_output = self.discrim(output_data)
+
+
+			bce = tf.keras.losses.BinaryCrossentropy(label_smoothing = 0.1, from_logits=True)
+			
+			discrim_output_loss_fake = tf.reduce_mean(
+			    discrim_fake_output
+			)
+	
+
+			discrim_output_loss_real = tf.reduce_mean(
+			    discrim_real_output
+			)
+
+			discrim_loss = discrim_output_loss_fake - discrim_output_loss_real
+		discrim_grads = tape.gradient(discrim_loss, self.discrim.trainable_weights)
+		discrim_grads,_ = tf.clip_by_global_norm(discrim_grads, 1.0)
+		self.discrim_optimizer.apply_gradients(zip(discrim_grads, self.discrim.trainable_weights))
+		self.discrim_loss_tracker.update_state(discrim_loss)
+		self.discrim_fake_loss_tracker.update_state(discrim_output_loss_fake)
+		self.discrim_real_loss_tracker.update_state(discrim_output_loss_real)
+		del tape
+					
+		with tf.GradientTape(persistent=True) as tape:
+			outputs = self.vrnn(input_data)
+
+			preds_mu = outputs[6]
+			preds_std = outputs[7]
+			gen = []
+			# (batch_size, seq_len, features_size)
+			# seed inputs, actually
+			inputs = input_data[:,0,:]
+			state = None
+
+			# gen can be the generated features or the sequence of latent codes
+
+			for i in range(100):
+				# outputs and states should be batch_size, feature_size
+				sample, state = self.vrnn.vrnn_cell(inputs, state, inference=False)
+				gen.append(sample[0])
+				inputs = sample[0] 
+				state = [state]
+			gen = tf.stack(gen, axis=1)
+
+			discrim_fake_output = self.discrim(gen)
+			
+			q_mu = outputs[2]
+
+			p_mu = outputs[3]
+			q_log_var = outputs[4]
+			p_log_var = outputs[5]
+
+			kl_loss = tf.reduce_mean(kl_gauss(q_mu, p_mu, q_log_var, p_log_var))
+			bce = tf.keras.losses.BinaryCrossentropy(label_smoothing = 0.1, from_logits=True)
+
+			reconstruction_loss = tf.reduce_mean(
+				nll_gauss(output_data, preds_mu, preds_std)
+			)
+			
+			
+			total_loss =  reconstruction_loss + 2.0 * kl_loss - discrim_fake_output
+			# total_loss =  self.lambda_gan * mislead_output_discrim_loss
+
+		encoder_grads = tape.gradient(total_loss, self.vrnn.trainable_weights)
+		# encoder_grads,_ = tf.clip_by_global_norm(encoder_grads, 10)
+		
+		self.vae_optimizer.apply_gradients(zip(encoder_grads, self.vrnn.trainable_weights))
+
+		self.total_loss_tracker.update_state(total_loss)
+		self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+		self.kl_loss_tracker.update_state(kl_loss)
+		del tape
+			
+		return {
+			'total_loss': self.total_loss_tracker.result(),
+			'loss': self.reconstruction_loss_tracker.result(),
+			'kl': self.kl_loss_tracker.result(),
+			'discrim_loss':self.discrim_loss_tracker.result(),
+			'discrim_loss_fake':self.discrim_fake_loss_tracker.result(),
+			'discrim_loss_real':self.discrim_real_loss_tracker.result(),
+		}
+
+	def call(self, inputs):
+		outputs = self.vrnn(inputs)
+		return outputs
+
+
+	def generate(self, inputs):
+		outputs = self.vrnn(inputs, inference=False)
+		return outputs
+	
+	def discrminator_score(self, inputs):
+		score = self.discrim(inputs)
+		return score 
+
+	def rec_gen(self, data, length):
+		state=None 
+		generated = []
+		# seed state
+		for i in range(data.shape[0]):
+			reshaped_data = np.asarray([data[i]])
+			outputs, state = self.vrnn.vrnn_cell(reshaped_data, states=state, inference=True)
+			state = [state]
+		gen_data = np.asarray([data[-1]])
+
+		for i in range(length):
+			outputs, s = self.vrnn.vrnn_cell(gen_data, states=state, inference=False)
+			# s += tf.random.normal(s.numpy().shape, 0, 0.3)
+			state = [s]
+			preds = outputs[0].numpy()
+			generated.append(preds)
+			gen_data = preds
+
+		return generated
+
+
+	
+	def test_step(self, data):
+		inputs = data[0]
+		outputs = data[1]
+		preds = self(inputs, training=False)
+		recon_loss = tf.keras.losses.categorical_crossentropy(outputs, preds)
+		return {
+			"loss": recon_loss
+		}
+
+	def vrnn_cell(self, input, state):
+		output, state = self.vrnn_cell(input, states=state, inference=False)
+		return output, state
